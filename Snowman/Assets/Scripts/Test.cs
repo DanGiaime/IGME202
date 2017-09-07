e394b3df6e7578eb795733f66fa1fbc8dfc3f712
Dan Giaime (RIT Student) <dsg6874@ad.rit.edu>
2017-09-07 10:52:27 -0400

    exploding snowmen

---
+++ Snowman/Assets/Scripts/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject top;
+    public GameObject middle;
+    public GameObject bottom;
+    public GameObject sceneManager;
+    private Count countingScript;
+
+    // Use this for initialization
+    void Start() {
+        sceneManager = GameObject.Find("SceneManager");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMouseDown()
+    {
+        top.AddComponent<Rigidbody>();
+        middle.AddComponent<Rigidbody>();
+        bottom.AddComponent<Rigidbody>();
+        Debug.Log("clicked");
+        countingScript = sceneManager.GetComponent<Count>();
+        countingScript.snowmanCounter++;
+        Destroy(gameObject.GetComponent<Collider>());
+    }
+}
